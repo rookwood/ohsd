15e9423136f4482cfb667bb5123044d554ceda76
Matt Hollis <matthollis@gmail.com>
2018-09-08 20:02:29 -0500

    Encounter statuses

--- app/Encounters/EncounterStatus.php
+++ app/Encounters/EncounterStatus.php
@@ -10,8 +10,32 @@ class EncounterStatus extends Status
     {
         return $query->whereNotNull('start_at')
             ->whereNull('arrived_at')
-            ->whereNull('departed_at')
-            ->whereNull('cancelled_at')
+            ->whereNull('cancelled_at');
+    }
+
+    public function arrived($query)
+    {
+        return $query->whereNotNull('start_at')
+            ->whereNotNull('arrived_at')
+            ->whereNull('departed_at');
+    }
+
+    public function departed($query)
+    {
+        return $query->whereNotNull('start_at')
+            ->whereNotNull('arrived_at')
+            ->whereNotNull('departed_at');
+    }
+
+    public function cancelled($query)
+    {
+        return $query->whereNotNull('cancelled_at')
             ->whereNull('rescheduled_to');
+    }
+
+    public function rescheduled($query)
+    {
+        return $query->whereNotNull('cancelled_at')
+            ->whereNotNull('rescheduled_to');
     }
 }
---
+++ tests/Unit/Encounters/EncounterStatusTest.php
@@ -0,0 +1,149 @@
+<?php
+
+namespace Tests\Unit\Encounters;
+
+use App\Encounters\Encounter;
+use App\Encounters\EncounterStatus;
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+
+class EncounterStatusTest extends TestCase
+{
+    use RefreshDatabase, WithFaker;
+
+    /** @test */
+    public function encounters_are_scheduled_when_patient_has_not_arrived_or_cancelled()
+    {
+    	$scheduledEncounter = factory(Encounter::class)->create([
+    	    'start_at' => $this->faker->dateTime,
+            'arrived_at' => null,
+            'cancelled_at' => null,
+        ]);
+
+    	$arrivedEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => $this->faker->dateTime,
+            'cancelled_at' => null,
+        ]);
+
+    	$cancelledEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => null,
+            'cancelled_at' => $this->faker->dateTime,
+        ]);
+
+    	$retrievedEncounters = Encounter::status(new EncounterStatus('scheduled'))->get();
+    	$this->assertCount(1, $retrievedEncounters);
+    	$this->assertTrue($retrievedEncounters->first()->is($scheduledEncounter));
+    }
+
+    /** @test */
+    public function encounters_are_arrived_when_patient_arrived_but_not_departed()
+    {
+    	$scheduledEncounter = factory(Encounter::class)->create([
+    	    'start_at' => $this->faker->dateTime,
+            'arrived_at' => null,
+            'cancelled_at' => null,
+        ]);
+
+    	$arrivedEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => $this->faker->dateTime,
+            'cancelled_at' => null,
+        ]);
+
+    	$departedEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => $this->faker->dateTime,
+            'departed_at' => $this->faker->dateTime,
+        ]);
+
+    	$retrievedEncounters = Encounter::status(new EncounterStatus('arrived'))->get();
+    	$this->assertCount(1, $retrievedEncounters);
+    	$this->assertTrue($retrievedEncounters->first()->is($arrivedEncounter));
+    }
+
+    /** @test */
+    public function encounters_are_departed_when_patient_arrived_and_then_departed()
+    {
+    	$scheduledEncounter = factory(Encounter::class)->create([
+    	    'start_at' => $this->faker->dateTime,
+            'arrived_at' => null,
+            'cancelled_at' => null,
+        ]);
+
+    	$arrivedEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => $this->faker->dateTime,
+            'cancelled_at' => null,
+        ]);
+
+    	$departedEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => $this->faker->dateTime,
+            'departed_at' => $this->faker->dateTime,
+        ]);
+
+    	$retrievedEncounters = Encounter::status(new EncounterStatus('departed'))->get();
+    	$this->assertCount(1, $retrievedEncounters);
+    	$this->assertTrue($retrievedEncounters->first()->is($departedEncounter));
+    }
+
+    /** @test */
+    public function encounters_are_cancelled_when_patient_has_cancelled_but_not_rescheduled()
+    {
+    	$scheduledEncounter = factory(Encounter::class)->create([
+    	    'start_at' => $this->faker->dateTime,
+            'arrived_at' => null,
+            'cancelled_at' => null,
+        ]);
+
+    	$rescheduledEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => $this->faker->dateTime,
+            'cancelled_at' => $this->faker->dateTime,
+            'rescheduled_to' => $this->faker->dateTime,
+        ]);
+
+        $cancelledEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => null,
+            'cancelled_at' => $this->faker->dateTime,
+            'rescheduled_to' => null,
+        ]);
+
+        $retrievedEncounters = Encounter::status(new EncounterStatus('cancelled'))->get();
+    	$this->assertCount(1, $retrievedEncounters);
+    	$this->assertTrue($retrievedEncounters->first()->is($cancelledEncounter));
+    }
+
+    /** @test */
+    public function encounters_are_rescheduled_when_patient_has_cancelled_and_rescheduled()
+    {
+        $scheduledEncounter = factory(Encounter::class)->create([
+            'start_at'     => $this->faker->dateTime,
+            'arrived_at'   => null,
+            'cancelled_at' => null,
+        ]);
+
+        $rescheduledEncounter = factory(Encounter::class)->create([
+            'start_at'       => $this->faker->dateTime,
+            'arrived_at'     => $this->faker->dateTime,
+            'cancelled_at'   => $this->faker->dateTime,
+            'rescheduled_to' => $this->faker->dateTime,
+        ]);
+
+        $cancelledEncounter = factory(Encounter::class)->create([
+            'start_at'       => $this->faker->dateTime,
+            'arrived_at'     => null,
+            'cancelled_at'   => $this->faker->dateTime,
+            'rescheduled_to' => null,
+        ]);
+
+        $retrievedEncounters = Encounter::status(new EncounterStatus('rescheduled'))->get();
+        $this->assertCount(1, $retrievedEncounters);
+        $this->assertTrue($retrievedEncounters->first()->is($rescheduledEncounter));
+    }
+
+}
